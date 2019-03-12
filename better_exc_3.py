e0afdb9c54f565a0641ce41e09aa02922d8457ee
Michał Wieluński <michal.wielunski@dcx.bz>
2019-03-12 11:48:13 +0100

    unblack

--- better_exc_3.py
+++ better_exc_3.py
@@ -1,13 +1,11 @@
 from jsonschema import validate
 
-schema = (
-    {
-        "type": "object",
-        "properties": {
-            "price": {"type": "number"},
-            "name": {"type": "string"},
-        },
+schema = {
+    "type": "object",
+    "properties": {
+        "price": {"type": "number"},
+        "name": {"type": "string"},
     },
-)
+},
 
 validate(instance={"name": "Eggs", "price": 36.6}, schema=schema)
